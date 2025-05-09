3da79c8accaadc2031da94567a801353b24b23ad
TranHuy0811 <huy08112004fo4@gmail.com>
2025-05-09 08:05:41 +0700

    edit

--- src/axios.jsx
+++ src/axios.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.API_URL, // Replace with your API base URL
+  baseURL: process.env.REACT_APP_API_URL, // Replace with your API base URL
 });
 
 export { axiosInstance};
